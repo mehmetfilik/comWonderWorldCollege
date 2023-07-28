887dd3077c98834b6808ef20074ff41c173d25b7
mehmetfilik <filikmehmet02gmail.com>
2023-07-28 15:08:22 +0200

    filik 28.07/v1

--- src/test/java/pages/CommonPage.java
+++ src/test/java/pages/CommonPage.java
@@ -1,4 +1,38 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CommonPage {
+
+    // userlogin > User Login Title
+    @FindBy(xpath = "(//h3)[1]")
+    private WebElement userLoginTitle;
+
+    // userlogin > What's New In Wonder World College Title
+    @FindBy(xpath = "(//h3)[2]")
+    private WebElement whatsNewTitle;
+
+    // userlogin > Username Textbox
+    @FindBy(id = "email")
+    private WebElement usernameTextbox;
+
+    // userlogin > Password Textbox
+    @FindBy(id = "password")
+    private WebElement passwordTextbox;
+
+    // userlogin > Sign In Button
+    @FindBy(xpath = "//*[@class='btn']")
+    private WebElement signInButton;
+
+    // userlogin > Forgot Password Button
+    @FindBy(xpath = "//*[@class='forgot']")
+    private WebElement forgotButton;
+
+    // userlogin > Front Site Button
+    @FindBy(linkText = "//div[2]/p[2]/a")
+    private WebElement frontSiteButton;
+
+
+
 }
