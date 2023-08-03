158c2f7eb8efeec0da970b3181b81b29b2d24d57
mehmetfilik <filikmehmet02gmail.com>
2023-08-03 23:15:28 +0200

    filik 03.08/v3

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources",
         glue = {"stepdefinitions"},
 
-        tags = "@bug",
+        tags = "@test",
 
         dryRun = false
 )
